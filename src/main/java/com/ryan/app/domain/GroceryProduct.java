85efa583a5f8f71c7a5748c4a26b18b4c21ab0b7
Ryan Le <55561793+guardiangod@users.noreply.github.com>
2026-01-07 23:38:09 +0800

    Add entities

---
+++ src/main/java/com/ryan/app/domain/GroceryProduct.java
@@ -0,0 +1,43 @@
+package com.ryan.app.domain;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroceryProduct extends Product {
+
+    private BigDecimal sellingPrice;
+    private BigDecimal weight;
+
+    private int expiryDate;
+
+    private int threshold;
+
+    private int availableStock;
+
+    private BigDecimal discount;
+
+    private GroceryStore store;
+
+    @Builder
+    public GroceryProduct(String productId, String productName, BigDecimal mrp, Cart cart,
+                          BigDecimal sellingPrice, BigDecimal weight, int expiryDate, int threshold,
+                          int availableStock, GroceryStore store, BigDecimal discount) {
+        super(productId, productName,  mrp);
+        this.sellingPrice = sellingPrice;
+        this.weight = weight;
+        this.expiryDate = expiryDate;
+        this.threshold = threshold;
+        this.availableStock = availableStock;
+        this.store = store;
+        this.discount = discount;
+    }
+
+}
